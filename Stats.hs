476ae77f54e4705ec17180565be4de5bfd98f71e
Joachim Breitner <mail@joachim-breitner.de>
2008-10-08 13:19:17 +0000

    Prelimary stats module

---
+++ Stats.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE ExistentialQuantification, PolymorphicComponents,
+             TemplateHaskell
+  #-}
+
+import Test.BenchPress
+import Control.Exception (evaluate)
+import Bff
+import Control.Monad.Writer
+import Control.Monad.State
+import Control.Monad.Fix
+import Data.Traversable hiding (mapM)
+import qualified Data.Traversable as T
+import qualified Data.Foldable as F 
+import Data.Zippable
+import Data.Zippable.Instances
+import Data.DeriveTH
+import Data.Derive.Zippable
+import Data.Derive.Traversable
+import Data.Derive.Functor
+import Data.List
+import Prelude
+import Data.Ord
+import Text.Printf
+import Graphics.Rendering.Chart
+
+-- Configuration
+sizes = [0,10^3..10^4]
+repetitions = 1
+
+data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
+$(derive makeZippable ''Tree)
+-- Lined, otherwise name clashes occur
+$(derive makeFunctor ''Tree)
+instance F.Foldable Tree where
+      foldr f b (Leaf a1) = (f a1 . id) b
+      foldr f b (Node a1 a2) = (flip (F.foldr f) a1 . (flip (F.foldr f) a2 . id)) b 
+$(derive makeTraversable ''Tree)
+
+
+data Test c c' a = Test
+	{ testName    :: String
+	, genTestCase :: Int  -> c  a
+        , getTest     :: forall a. c a  -> c' a -- Polymorphic type important!
+	, putTest     :: c a  -> c' a -> c a
+        , modifyTest  :: c' a -> c' a
+	}
+
+test1 = Test
+	{ testName    = "Halve"
+	, genTestCase = \n -> [1..n]
+	, getTest     = \as -> take (length as `div` 2) as
+	, putTest     = \as as' -> as' ++ drop (length as `div` 2) as
+	, modifyTest  = pairDance
+	}
+
+pairDance [] = []
+pairDance [x] = [x]
+pairDance (a:b:r) = (b:a:pairDance r)
+
+test2 :: Test Tree [] ()
+test2 = Test
+	{ testName   = "Flatten"
+	, genTestCase = fix (\loop n -> if n <= 1
+                                        then Leaf ()
+                                        else Node (loop (n`div`2)) (loop (n - (n`div`2))))
+	, getTest     = execWriter . T.mapM (tell . (:[])) 
+	, putTest     = \s v -> flip evalState v $
+				    T.mapM (\_ -> do new <- gets head
+						     modify tail
+						     return new) s
+	, modifyTest  = pairDance
+	}
+
+stats test putter size = (mean . fst) `fmap` benchmark repetitions
+		(do let source = genTestCase test size
+		        view   = getTest test source
+                        view'   = modifyTest test view
+                    deepEvaluate (source, view')
+                )
+		(\_ -> return ())
+		(\(source, view') -> deepEvaluate (putter test source view')
+		)
+
+manualPutter test = putTest test
+
+bffPutter test = bff (getTest test)
+
+deepEvaluate :: Show a => a -> IO a
+deepEvaluate x = evaluate (length (show x)) >> return x
+
+collectStats test = mapM (\size -> do
+		manual <- stats test manualPutter size
+	        automatic <- stats test bffPutter size
+                return (size, manual, automatic)
+	) sizes
+
+putTable statData = putStr (tabelize stringData)
+  where stringData = ["Size","Manual Getter", "Automatic Getter"] :
+                     map (\(s,m,a) -> [show s, printf "%.3f" m, printf "%.3f" a]) statData
+
+tabelize :: [[String]] -> String
+tabelize table = unlines (map padLine table)
+  where colWidths = map (maximum . map length) (transpose table)
+        pad w s = replicate (w - length s) ' ' ++ s
+	padLine ss = intercalate "|" (zipWith pad colWidths ss)
+
+putGraph test statData filename = renderableToPNGFile r 800 400 filename
+  where r = toRenderable $
+		defaultLayout1
+		{ layout1_title = "Test \"" ++ testName test ++"\""
+		, layout1_plots = [
+			("Manual Getter", HA_Bottom, VA_Left,
+				toPlot $ defaultPlotLines 
+					{ plot_lines_values = [manualData]
+					, plot_lines_style = solidLine 1 (Color 0 0 1)
+					}
+			),
+			("Automatic Getter", HA_Bottom, VA_Left,
+				toPlot $ defaultPlotLines 
+					{ plot_lines_values = [automaticData]
+					, plot_lines_style = solidLine 1 (Color 1 0 0)
+					}
+			)
+			]
+		, layout1_vertical_axes = 
+			linkedAxes' (autoScaledAxis (defaultAxis {axis_title =
+					"Average time per test in ms"
+				}))
+		, layout1_horizontal_axes = 
+			linkedAxes' (autoScaledAxis (defaultAxis {axis_title =
+					"Size of input data structure"
+				}))
+		}
+        (manualData, automaticData) = unzip $ map f statData
+	f (s, m, a) = (Point (fromIntegral s) m, Point (fromIntegral s) a)
+
+runTest :: (Show (c v), Show (c' v), F.Foldable c', Zippable c', Traversable c, Eq v) =>
+           Test c c' v -> IO ()
+runTest test = do
+	  statData <- collectStats test
+	  putStrLn $ "Test \"" ++ testName test ++ "\":"
+	  putTable statData
+	  let graphFileName = testName test ++ ".png"
+	  putStrLn $ "Writing graph to " ++ graphFileName
+	  putGraph test statData graphFileName
+
+main = do putStrLn $ "Bff benchmarking program"
+          putStrLn $ "(c) 2008 Joachim Breitner"
+	  putStrLn $ "Repeating every test " ++ show repetitions ++ " times."
+	  putStrLn $ "" 
+          runTest test1
+	  runTest test2
