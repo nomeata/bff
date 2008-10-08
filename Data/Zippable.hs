bc463034ff5263e05069305f4f5e79df104c5d8e
Joachim Breitner <mail@joachim-breitner.de>
2008-10-08 08:03:45 +0000

    Add tryZipWith' to Zippable class, with custom CError Monad

--- Data/Zippable.hs
+++ Data/Zippable.hs
@@ -1,7 +1,21 @@
+{-# LANGUAGE ExistentialQuantification, PolymorphicComponents #-}
 module Data.Zippable where
 
+newtype CError a = CError (forall b. (a -> Either String b) -> Either String b)
+
+instance Monad CError where
+	return a = CError (\h -> h a)
+ 	(CError p) >>= k = CError (\h -> p (\a -> case k a of CError q -> q h))
+
+cError err = CError (const (Left err))
+
+cErrorToEither :: CError a -> Either String a
+cErrorToEither (CError p) = p Right
+
 class Zippable k where
-  tryZipWith :: (a -> b -> Either String c) ->  k a -> k b -> Either String (k c)
+  tryZipWith' :: (a -> b -> CError c) ->  k a -> k b -> CError (k c)
+  tryZipWith :: (a -> b -> c) ->  k a -> k b -> Either String (k c)
+  tryZipWith combiner  x y = cErrorToEither (tryZipWith' (\x y -> return (combiner x y)) x y)
   tryZip :: k a -> k b -> Either String (k (a,b))
-  tryZip = tryZipWith (\a b -> Right (a,b))
+  tryZip = tryZipWith (,)
 
