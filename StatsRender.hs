59e010674481408a6614c42fbe3520d51f2eb38e
Joachim Breitner <mail@joachim-breitner.de>
2008-10-29 12:40:13 +0000

    Janis’ style changes

--- StatsRender.hs
+++ StatsRender.hs
@@ -7,44 +7,46 @@ import System.IO
 import Data.List
 import StatsDef
 
-putGraph name statData filename = renderableToPDFFile r 800 400 filename
+
+putGraph name statData filename = renderableToPDFFile r 600 400 filename
   where r = toRenderable $
 		defaultLayout1
-		{ layout1_title = "Test \"" ++ name ++"\""
+		{ layout1_title = "Measurements \"" ++ name ++"\""
 		, layout1_plots = [
-			("Manual Getter", HA_Bottom, VA_Left,
+			("manual (put)", HA_Bottom, VA_Left,
 				toPlot $ defaultPlotLines 
 					{ plot_lines_values = [manualData]
-					, plot_lines_style = solidLine 1 (Color 0 0 1)
+					, plot_lines_style = solidLine 1 (Color 0 0 0)
 					}
 			),
-			("Automatic Getter", HA_Bottom, VA_Left,
+			("automatic (bff get)", HA_Bottom, VA_Left,
 				toPlot $ defaultPlotLines 
 					{ plot_lines_values = [automaticData]
-					, plot_lines_style = solidLine 1 (Color 1 0 0)
+					, plot_lines_style = dashedLine 1 [10,10] (Color 0 0 0)
 					}
 			)
 			]
 		, layout1_vertical_axes = 
 			linkedAxes' (autoScaledAxis (defaultAxis {axis_title =
-					"Average time per test in ms"
+					"Average time per run in ms"
 				}))
 		, layout1_horizontal_axes = 
 			linkedAxes' (autoScaledAxis (defaultAxis {axis_title =
-					"Size of input data structure"
+					"Size of original source"
 				}))
 		}
         (manualData, automaticData) = unzip $ map f statData
 	f (s, m, a) = (Point (fromIntegral s) m, Point (fromIntegral s) a)
 
-
 printTest :: String -> StatRunData -> IO ()
 printTest name statData = do
-	  putStr   $ "Test \"" ++ name ++ "\" "
+	  putStr   $ "Measurements \"" ++ name ++ "\" "
 	  let graphFileName = name ++ ".pdf"
 	  putStrLn $ "Writing graph to " ++ graphFileName
 	  putGraph name statData graphFileName
 
-
 main = do cont <- readFile "stats.data"
 	  mapM_ (uncurry printTest) (read cont)
+
+
+
